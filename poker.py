2a7136077290d1feeaecb24102423ae6a8d839e7
itb-ie <42895988+itb-ie@users.noreply.github.com>
2018-10-30 00:11:12 +0100

    Poker simulator

---
+++ poker.py
@@ -0,0 +1,87 @@
+import random
+suits = ["♠", "♥", "♦", "♣"]
+ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+
+class Card(object):
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def get_suit(self):
+        return self.suit
+
+    def get_rank(self):
+        return self.rank
+
+    def __str__(self):
+        return "%s%s" %(self.rank, self.suit)
+
+
+class Deck(object):
+    def __init__(self):
+        self.cards = []
+        for s in suits:
+            for r in ranks:
+                self.cards.append(Card(s, r))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def __str__(self):
+        deck = ""
+        for i in range(0, 52):
+            deck += str(self.cards[i]) + " "
+        return deck
+
+    def take_one(self):
+        return self.cards.pop(0)
+
+
+class Hand(object):
+    def __init__(self, deck):
+        self.cards = []
+        for i in range(5):
+            self.cards.append(deck.take_one())
+
+    def __str__(self):
+        hand = ""
+        for i in range(5):
+            hand += str(self.cards[i]) + " "
+        return hand
+
+    def is_pair(self):
+        for i in range(5):
+            for j in range(i+1, 5):
+                if self.cards[i].get_rank() == self.cards[j].get_rank():
+                    return True
+        return False
+
+    def is_three(self):
+        for i in range(5):
+            for j in range(i+1, 5):
+                for k in range(j+1, 5):
+                    if self.cards[i].get_rank() == self.cards[j].get_rank() and self.cards[i].get_rank() == self.cards[k].get_rank():
+                        return True
+        return False
+
+new_deck = Deck()
+print(new_deck)
+new_deck.shuffle()
+print(new_deck)
+print("Taking out a card:", str(new_deck.take_one()))
+count = 0
+while True:
+    count += 1
+    new_deck = Deck()
+    new_deck.shuffle()
+    hand = Hand(new_deck)
+    if hand.is_three():
+        print("we have three of a kind, took us %s tries:" % count)
+        print(hand)
+        break
+    if count % 1000 == 0:
+        print(str(count) + "tries")
+
+
+
